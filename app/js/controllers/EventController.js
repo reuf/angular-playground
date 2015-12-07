f32db2bbc4df6c5c98083d6eddf5bfdb2487f4dc
muhamed.halilovic <muhamed.halil@gmail.com>
2015-12-07 18:30:48 +0100

    First commit

---
+++ app/js/controllers/EventController.js
@@ -0,0 +1,40 @@
+'use strict';
+
+eventsApp.controller('EventController',
+	function EventController($scope){
+		$scope.event = {
+			name: 'Angular Boot Camp',
+			date: '1/1/2013',
+			time: '10:30 am',
+			location: {
+				address: 'Google Headquarters',
+				city: 'Mountin View',
+				province: 'CA'
+			},
+			imageUrl:'/img/angularjs-logo.png',
+			sessions: [
+				{
+					name: 'Directives MasterClass',
+					creatroName: 'John Doe.',
+					duration: '1 h',
+					level: 'Advanced',
+					abstract: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident, dolorum.'
+				},
+				{
+					name: 'Scopes for fun and profiut.',
+					creatroName: 'Marc Zuckenberg.',
+					duration: '1h',
+					level: 'Beginner',
+					abstract: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, beatae?'
+				},
+				{
+					name: 'Well Behaved Controllers',
+					creatroName: 'Halil Halilovic.',
+					duration: '2h',
+					level: 'Beginner',
+					abstract: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, voluptatibus.'
+				}
+			]
+		}
+	}
+);
